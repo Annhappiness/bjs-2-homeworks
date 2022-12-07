68e57cbe1051da040e4628f5a7e41bb926143eb2
Annhappiness <111363993+Annhappiness@users.noreply.github.com>
2022-12-07 17:42:19 +0300

    Update task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -28,27 +28,28 @@ function solveEquation(a, b, c) {
 
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
-  let per = parseInt(percent) / 100 / 12; 
-  let con = parseInt(contribution); 	  
-  let am = parseInt(amount);  			  
-  if (isNaN(per) || per < 0) {
-     return false;
-   } else if (isNaN(con) || con < 0) {
-     return false;
-   } else if (isNaN(am) || am < 0) {
-     return false;
-   } else {
-    
-    let today = new date();
-    if (today.getFullYear() > date.getFullYear() || today.getMonth() > date.getMonth()) {
-    return false;
-  } else {
-    let s = am - con;						 
-      let n = date.getMonth() - today.getMonth() + (12*(date.getFullYear() - today.getFullYear())); 
-      let pay = s*(per+per/(((1+per)**n)-1));			
-      let totalAmount = (pay * n).toFixed(2);
-      console.log(totalAmount);
-     return +totalAmount;
-   }	
-  }
+  let per = parseInt(percent) / 100 / 12; //процентная ставка
+    let con = parseInt(contribution); 	  //начальный взнос	
+    let am = parseInt(amount);  			  //общая стоимость	
+  	if (isNaN(per) || per < 0) {
+   		return `Параметр "Процентная ставка" содержит неправильное значение "${percent}"`;
+   	} else if (isNaN(con) || con < 0) {
+   		return `Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`;
+   	} else if (isNaN(am) || am < 0) {
+   		return `Параметр "Общая стоимость" содержит неправильное значение "${amount}"`;
+   	} else {
+
+  		let today = new Date();
+  		if (today.getFullYear() > date.getFullYear() || today.getMonth() > date.getMonth()) {
+			return `Параметр "срок ипотеки" содержит неправильное значение ${date}`;
+		} else {
+			let s = am - con;						  //тело кредита
+  			let n = date.getMonth() - today.getMonth() + (12*(date.getFullYear() - today.getFullYear())); //срок кредита в месяцах
+  			let pay = s*(per+per/(((1+per)**n)-1));			//ежемесячная оплата
+  			let totalAmount = (pay * n).toFixed(2);
+  			console.log(totalAmount);
+ 			return +totalAmount;
+ 		}	
+    }
+
 }
